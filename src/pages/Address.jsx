998ec50f5b5a96762154047bcb6f0a4fdb2f3fce
Saks05 <sonisakshi2006@gmail.com>
2024-04-02 17:46:47 +0530

    Checkboxes

--- src/pages/Address.jsx
+++ src/pages/Address.jsx
@@ -3,6 +3,7 @@ import { Margin, VisibilityOff } from "@mui/icons-material";
 import {
   Box,
   Button,
+  Checkbox,
   FormControl,
   Grid,
   InputAdornment,
@@ -25,10 +26,10 @@ import React, { useEffect, useState } from "react";
 import AddressForm from "../components/AddressForm";
 import ManageSearchIcon from '@mui/icons-material/ManageSearch';
 import dummyData from "../constants/JSONFILE1.json";
-
+import dummyData2 from "../constants/JSONFile2.json";
 
 const Address = () => {
-  
+
   const style = {
     position: "absolute",
     top: "50%",
@@ -39,20 +40,20 @@ const Address = () => {
     border: "2px solid #000",
     boxShadow: 24,
     p: 4,
-    
+
   };
   const [open, setOpen] = useState(false);
   const handleOpen = () => setOpen(true);
   const handleClose = () => setOpen(false);
-  const [formType,setFormType] = useState("");
-  const [selectedItem,setSelectedItem] = useState({})
-  const [country,setCountry]=useState("india")
+  const [formType, setFormType] = useState("");
+  const [selectedItem, setSelectedItem] = useState({})
+  const [country, setCountry] = useState("india")
   const [page, setPage] = useState(0);
   const [rowsPerPage, setRowsPerPage] = useState(5);
   const handleChangePage = (event, newPage) => {
     setPage(newPage);
   };
-
+  const [commonElements, setCommonElements] = useState([]);
   const handleChangeRowsPerPage = (event) => {
     setRowsPerPage(parseInt(event.target.value, 10));
     setPage(0);
@@ -60,32 +61,77 @@ const Address = () => {
   const handleSearch = (value) => {
     console.log("ye tera parameter hai bhai", value);
     if (value !== "") {
-        const newArray = dummyData.filter((item) => (
-            item.address_code.toLowerCase().includes(value.toLowerCase()) || 
-            item.name.toLowerCase().includes(value.toLowerCase())
-        ));
-        if (newArray.length > 0) { // Changed newArray.length > 1 to newArray.length > 0
-            console.log("newArray ko handleSearch me console kiya h",setFilterData(newArray));
-        }
+      const newArray = dummyData.filter((item) => (
+        item.address_code.toLowerCase().includes(value.toLowerCase()) ||
+        item.name.toLowerCase().includes(value.toLowerCase())
+      ));
+      if (newArray.length > 0) { // Changed newArray.length > 1 to newArray.length > 0
+        console.log("newArray ko handleSearch me console kiya h", setFilterData(newArray));
+      }
     } else {
-        setPage(1);
+      setPage(1);
     }
-}
-
-  const [filteredData,setFilterData]= useState([]);
-  useEffect(()=>{
-
-    console.log("og array", dummyData) ;
-    // const newArray=[...dummyData];
-    const newArray= dummyData.slice(page*rowsPerPage, page*rowsPerPage + rowsPerPage,);
-    
-    console.log("set filter ", setFilterData(newArray)) ;
-    
-    console.log ("start", page * rowsPerPage)
-    console.log ("end ",  page * rowsPerPage + rowsPerPage)
-    }, [ page, rowsPerPage] )
+  };
+  const handleSearchCountry = (value) => {
+    console.log("country handleSearch",value);
+    if(value!==""){
+      const newArray = dummyData.filter((item) => (
+        item.country.toLowerCase().includes(value.toLowerCase())
+      ));
+      if (newArray.length > 0) { // Changed newArray.length > 1 to newArray.length > 0
+        console.log("cuntryHAndle search newArray", setFilterData(newArray));
+      }
+    } else {
+      setPage(1);
+    } 
+    };
   
+  const commonElement = (value) => {
+    let found = false;
+    commonElements.forEach(element => {
+      if (element === value) {
+        found = true;
+      }
+    });
+    return found;
+  };
+  console.log("common elemets jii",commonElements);
+  const [filteredData, setFilterData] = useState([]);
+  useEffect(() => {
 
+
+    const newArray = dummyData.slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage,);
+
+    console.log("set filter ", setFilterData(newArray));
+
+    console.log("start", page * rowsPerPage)
+    console.log("end ", page * rowsPerPage + rowsPerPage)
+  }, [page, rowsPerPage])
+
+  
+  useEffect(() => {
+    const newArray = [];
+    console.log('dummyData', dummyData)
+    console.log('dummyData2', dummyData2)
+   
+    dummyData.forEach((element1, index) => {
+
+      dummyData2.forEach((element2, index) => {
+
+        if (JSON.stringify(element1) === JSON.stringify(element2)) {
+          console.log("ye element same hai bhai dono mai", element1)
+
+          newArray.push(element1.address_code)
+        }
+
+      })
+
+    }
+    )
+    setCommonElements(newArray)
+    commonElement("abc")
+  }, [])
+  console.log(commonElement("abc")); 
 
   return (
     <Box sx={{ height: "100%", width: "100%", marginTop: "10px" }}>
@@ -97,55 +143,59 @@ const Address = () => {
         }}
       >
         <Typography variant="h5">Address</Typography>
-        <Button variant="contained" onClick={()=>{handleOpen();setFormType("new")}} >
-          Add New Address 
-         
+        <Button variant="contained" onClick={() => { handleOpen(); setFormType("new") }} >
+          Add New Address
+
         </Button>
       </Box>
       <Grid container spacing={2}>
         <Grid item xs={12} sm={8} md={8}>
-          <Box sx={{marginTop:"12px"}}>
-          <FormControl variant="outlined" fullWidth>
-          <InputLabel htmlFor="outlined-adornment-search">Search Address Code, Name</InputLabel>
-          <OutlinedInput
-            id="outlined-adornment-search"
-            type='text' 
-             onChange={(e)=>{handleSearch(e.target.value)}}
-            endAdornment={
-              <InputAdornment position="end">
-                
-            <ManageSearchIcon/>
-              </InputAdornment>
-            }
-            label="Search Address Code, Name"
-          />
-        </FormControl>
+          <Box sx={{ marginTop: "12px" }}>
+            <FormControl variant="outlined" fullWidth>
+              <InputLabel htmlFor="outlined-adornment-search">Search Address Code, Name</InputLabel>
+              <OutlinedInput
+                id="outlined-adornment-search"
+                type='text'
+                onChange={(e) => { handleSearch(e.target.value) }}
+                endAdornment={
+                  <InputAdornment position="end">
+
+                    <ManageSearchIcon />
+                  </InputAdornment>
+                }
+                label="Search Address Code, Name"
+              />
+            </FormControl>
           </Box>
         </Grid>
         <Grid item xs={12} sm={4} md={4}>
-          
-        <Box sx={{marginTop:"12px"}} > 
-        <FormControl fullWidth>
-  <InputLabel id="demo-simple-select-label">Country</InputLabel>
-  <Select
-    labelId="demo-simple-select-label"
-    id="demo-simple-select"
-    value={country}
-    label="Country"
-    onChange={(e)=>{setCountry(e.target.value)}}
-  >
-    <MenuItem value={"India"}>India</MenuItem>
-    {/* <MenuItem value={"USA"}>USA</MenuItem> */}
-  </Select>
-</FormControl>
-        </Box>
-          
+
+          <Box sx={{ marginTop: "12px" }} >
+            <FormControl fullWidth>
+              <InputLabel id="demo-simple-select-label">Country</InputLabel>
+              <Select
+                labelId="demo-simple-select-label"
+                id="demo-simple-select"
+                value={country}
+                label="Country"
+                onChange={(e) => { handleSearchCountry(e.target.value);setCountry(e.target.value) }}
+              >
+                <MenuItem value={"India"}>India</MenuItem>
+                <MenuItem value={"USA"}>USA</MenuItem>
+                <MenuItem value={"Pakistan"}>Pakistan</MenuItem>
+                <MenuItem value={"Indonesia"}>Indonesia</MenuItem>
+
+
+              </Select>
+            </FormControl>
+          </Box>
+
         </Grid>
-        </Grid>
-        
-        
-       
-      
+      </Grid>
+
+
+
+
       <Box>
         <TableContainer component={Paper} sx={{ marginTop: "15px" }}>
           <Table sx={{ minWidth: 650 }} aria-label="simple table">
@@ -166,7 +216,7 @@ const Address = () => {
                   sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
                 >
                   <TableCell component="th" scope="row">
-                    <Button variant="text" onClick={()=>{handleOpen();setFormType("update");setSelectedItem(row); console.log(row,"yeh row h")}}>
+                    <Button variant="text" onClick={() => { handleOpen(); setFormType("update"); setSelectedItem(row); console.log(row, "yeh row h") }}>
                       {row.address_code}
                     </Button>
                   </TableCell>
@@ -175,31 +225,32 @@ const Address = () => {
                   <TableCell align="right">{row.city}</TableCell>
                   <TableCell align="right">{row.country}</TableCell>
                   <TableCell align="right">{row.email}</TableCell>
+                  <TableCell align="right"><Checkbox  disabled checked={commonElement(row.address_code)} /></TableCell>
                 </TableRow>
               ))}
             </TableBody>
           </Table>
           <TablePagination
-          rowsPerPageOptions={[5, 10, 25]}
-          component="div"
-          count={dummyData.length}
-          rowsPerPage={rowsPerPage}
-          page={page}
-          onPageChange={handleChangePage}
-          onRowsPerPageChange={handleChangeRowsPerPage}
-        />
+            rowsPerPageOptions={[5, 10, 25]}
+            component="div"
+            count={dummyData.length}
+            rowsPerPage={rowsPerPage}
+            page={page}
+            onPageChange={handleChangePage}
+            onRowsPerPageChange={handleChangeRowsPerPage}
+          />
         </TableContainer>
       </Box>
       <Box>
         <Modal
           open={open}
           onClose={handleClose}
-          
+
           aria-labelledby="modal-modal-title"
           aria-describedby="modal-modal-description"
         >
           <Box sx={style}>
-            <AddressForm onClose={handleClose} formType={formType} selectedItem={selectedItem}/>
+            <AddressForm onClose={handleClose} formType={formType} selectedItem={selectedItem} />
           </Box>
         </Modal>
       </Box>
